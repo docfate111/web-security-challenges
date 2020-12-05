a71edfa65a072d8f6058f66f5dc62b86363418ee
docfate111 <tdwilliamsiv@gmail.com>
2020-12-05 17:33:56 -0600

    node async

--- cs253-websec-stanford/assign0/src/learnyounode/http-collect.js
+++ cs253-websec-stanford/assign0/src/learnyounode/http-collect.js
@@ -1,1 +1,22 @@
-// TODO: Replace this with your solution.
+// Write a program that performs an HTTP GET request to a URL provided to you  
+// as the first command-line argument. Collect all data from the server (not  
+// just the first "data" event) and then write two lines to the console  
+// (stdout).  
+const http = require('http');
+const url = process.argv[2];
+function callback (response) { 
+    var result = '';
+    response.setEncoding("utf8");
+    response.on('error', function (error) { console.error(error) });
+    response.on('data', function (data) {
+         result += data;
+        });
+    response.on('end', function(){ 
+        console.log(result.length);
+        console.log(result);
+    })
+}
+http.get(url, callback);
+// The first line you write should just be an integer representing the number  
+// of characters received from the server. The second line should contain the  
+// complete String of characters sent by the server.  
--- cs253-websec-stanford/assign0/src/learnyounode/juggling-async.js
+++ cs253-websec-stanford/assign0/src/learnyounode/juggling-async.js
@@ -1,1 +1,22 @@
-// TODO: Replace this with your solution.
+// This problem is the same as the previous problem (HTTP COLLECT) in that  
+//   you need to use http.get(). However, this time you will be provided with  
+//   three URLs as the first three command-line arguments.  
+   
+//   You must collect the complete content provided to you by each of the URLs  
+//   and print it to the console (stdout). You don't need to print out the  
+//   length, just the data as a String; one line per URL. The catch is that you  
+//   must print them out in the same order as the URLs are provided to you as  
+//   command-line arguments. 
+const http = require('http');
+const bl = require('bl');
+const async = require('async');
+const urls = process.argv.splice(2); // ignore first 2 args
+async.eachSeries(urls, (url, callback) => {
+        http.get(url, (response) => { 
+            response.pipe(bl((err, data) => {
+                if(err){ return console.error(err); }
+                console.log(data.toString());
+            }));
+        });
+        callback();
+    });
